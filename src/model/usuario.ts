dd9fefae0044a5d514ebd767a030d8e3a2613ecd
George Graciosa <george.graciosa@gmail.com>
2026-07-27 21:26:40 -0300

    fix: ajustado o modelo usuario

--- src/model/usuario.ts
+++ src/model/usuario.ts
@@ -2,20 +2,23 @@ export class Usuario {
   id?: number
   nome: string
   email: string
-  senha?: string
-  tipo: string
+  login: string
+  senha: string
+  cpf: string
 
   constructor(
     nome: string,
     email: string,
-    senha?: string,
-    tipo: string = 'comum',
+    login: string,
+    senha: string,
+    cpf: string,
     id?: number
   ) {
     this.nome = nome
     this.email = email
+    this.login = login
     this.senha = senha
-    this.tipo = tipo
+    this.cpf = cpf
     this.id = id
   }
 }
