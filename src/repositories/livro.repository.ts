dde6e8619b31fda4fe740650f9b7f0ddcb58a4ec
george.graciosa <george.graciosa@nsinova.com.br>
2026-07-29 11:28:07 -0300

    feat: criado repositorio de livros e excluida a pasta duplicada de database

---
+++ src/repositories/livro.repository.ts
@@ -0,0 +1,32 @@
+import {Pool} from 'pg'
+import {Livro} from '../model/livro'
+
+export class LivroRepository {
+
+    constructor(private readonly pool: Pool){}
+
+    async buscarPorId(id: number): Promise<Livro | null>{
+        const {rows} = await this.pool.query(
+            'SELECT * FROM livro WHERE id = $1',
+            [id]
+        )
+
+        if(rows.length === 0) null
+
+        const linha = rows[0]
+
+        return new Livro (
+            linha.titulo,
+            linha.editora_id,
+            linha.categoria_id,
+            linha.quantidade_disponivel,
+            linha.id
+        )
+    }
+    async atualizarEstoque(id: number, novaQuantidade: number): Promise<void>{
+        await this.pool.query(
+            'UPDATE livro SET quantidade_disponuvel = $1 WHERE id = $2',
+            [novaQuantidade, id]
+        )
+    }
+}
