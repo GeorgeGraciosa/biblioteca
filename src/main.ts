881bd3f3e0211bc2ac8cb0ba043fb0d245499113
george.graciosa <george.graciosa@nsinova.com.br>
2026-07-29 14:13:26 -0300

    feat: ajustado menu com as funcionalidades de reservar livro e cadastrar usuario

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { initDatabase, pool } from './@common/database/database'
 import { UserRepository } from './repositories/user.repository'
 import { CreateUserUseCase } from './usecase/create-user.uc'
 import { MainView } from './view/main.view'
+import { LivroRepository } from './repositories/livro.repository'
+import { ReservaUseCase } from './usecase/reserva.usecase'
 
 async function bootstrap() {
   await initDatabase()
 
   const createUserUc = new CreateUserUseCase(new UserRepository(pool))
-  const mainView = new MainView(createUserUc)
+  const livroRepository = new LivroRepository(pool)
+  const reservaUseCase = new ReservaUseCase(livroRepository)
+  const mainView = new MainView(createUserUc, reservaUseCase)
 
   await mainView.start()
 }
