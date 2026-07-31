9d00f57272f2829f927da676bbdb1852c52c986f
George Graciosa <george.graciosa@gmail.com>
2026-07-30 22:17:09 -0300

    chore: adiciona script seed.sql com dados de teste

--- src/repositories/reserva.repository.ts
+++ src/repositories/reserva.repository.ts
@@ -1,31 +1,32 @@
-import { Pool } from "pg";
-import { ReservaAcervo } from "../model/reserva-acervo";
+import { Pool } from 'pg'
+import { ReservaAcervo } from '../model/reserva-acervo'
 
 export class ReservaRepository {
-    constructor(private readonly pool: Pool){}
+  constructor(private readonly pool: Pool) {}
 
-    async buscarPorId(id: number): Promise<ReservaAcervo | null>{
-        const {rows} = await this.pool.query(
-            'SELECT * FROM reservas WHERE id = $1',
-            [id]
-        )
-        if(rows.length === 0) return null
+  async buscarPorId(id: number): Promise<ReservaAcervo | null> {
+    const { rows } = await this.pool.query(
+      'SELECT * FROM reserva_acervo WHERE id = $1',
+      [id]
+    )
+    if (rows.length === 0) return null
 
-        const linha = rows[0]
+    const linha = rows[0]
 
-        return new ReservaAcervo(
-            linha.livro_id,
-            linha.usuario_id,
-            linha.data_reserva,
-            linha.status,
-            linha.id
-        )
-    }
+    return new ReservaAcervo(
+      linha.usuario_id,
+      linha.livro_id,
+      linha.status,
+      linha.data_reserva,
+      linha.data_devolucao,
+      linha.id
+    )
+  }
 
-    async atualizarStatus(id: number, novoStatus: string): Promise<void>{
-        await this.pool.query(
-            'UPDATE reservas SET status = $1 WHERE id = $2',
-            [novoStatus, id]
-        )
-    }
+  async atualizarStatus(id: number, novoStatus: string): Promise<void> {
+    await this.pool.query(
+      'UPDATE reserva_acervo SET status = $1 WHERE id = $2',
+      [novoStatus, id]
+    )
+  }
 }
